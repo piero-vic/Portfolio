ac7d14634e40e4fbabbb8210125b355f54a00922
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-04 18:03:26 -0500

    Add changeButton function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+function changeButton() {
+  const hamburgerButton = document.getElementById('hamburger-btn');
+  if (hamburgerButton.getAttribute('src') === 'assets/hamburger.svg') {
+    hamburgerButton.setAttribute('src', 'assets/cancel.svg');
+  } else {
+    hamburgerButton.setAttribute('src', 'assets/hamburger.svg');
+  }
+}
+
 function toggle() {
   const mobileMenu = document.getElementById('mobile-nav-list');
   mobileMenu.classList.toggle('toggle-navbar');
+  changeButton();
 }
 
 // Add event listener to navigation links
