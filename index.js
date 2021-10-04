6b486bd33dbd7247d0115284d769fff5dd96503e
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-04 17:44:37 -0500

    Add event listener to nav links

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function toggle(btn){
-    const mobileMenu = document.getElementById('mobile-nav-list')
-    mobileMenu.classList.toggle('toggle-navbar')
-
-
-    console.log(mobileMenu)
+function toggle() {
+  const mobileMenu = document.getElementById('mobile-nav-list');
+  mobileMenu.classList.toggle('toggle-navbar');
 }
+
+// Add event listener to navigation links
+const navLinks = Array.from(document.getElementsByClassName('mobile-nav-link'));
+navLinks.forEach((element) => element.addEventListener('click', toggle));
