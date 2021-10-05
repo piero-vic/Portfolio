fa8bf2d2c331f191eecf4f5e51262b76649f8af0
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-05 12:44:07 -0500

    Complete createCard function

--- index.js
+++ index.js
@@ -24,55 +24,68 @@ let projects = [
     description: "Portfolio website to showcase my work. Code is on GitHub. Feel free to check it out.",
     technologies: ['HTML','CSS'],
     image: 'desktop-screenshot.png',
-    repoLink: "https://github.com/piero-vic/Portfolio"
+    repoLink: "https://github.com/piero-vic/Portfolio",
+    cardId: 0,
   },
   {
     name: "crypto-prices",
     description: "A script that requests crypto prices from CoinGecko and writes them to a file in the format required by the Ledger CLI price history.",
     technologies: ['Python','Ledger CLI', 'CoinGecko API'],
     image: 'assets/crypto.jpeg',
-    repoLink: "https://github.com/piero-vic/crypto-prices"
+    repoLink: "https://github.com/piero-vic/crypto-prices",
+    cardId: 1,
   },
   {
     name: "bootstrap-website",
     description: "Basic presentation website. Made with Bootstrap.",
     technologies: ['HTML','Bootstrap'],
     image: 'assets/bootstrap-webpage.png',
-    repoLink: "https://github.com/piero-vic/bootstrap-website"
+    repoLink: "https://github.com/piero-vic/bootstrap-website",
+    cardId: 2,
   },
   {
     name: "Patches: A Collection of VCV Rack Patches",
     description: "VCV Rack is a virtual modular synthesizer. All preset are text files in JSON format so they work great with version control.",
     technologies: ['JSON','VCV Rack'],
     image: 'assets/synth-unsplash.jpeg',
-    repoLink: "https://github.com/piero-vic/patches"
+    repoLink: "https://github.com/piero-vic/patches",
+    cardId: 3,
   }
 ]
 
-function createCard() {
+function createCard(project) {
   const workSection = document.getElementById('work-section');
   // Card
   const card = document.createElement('div');
-  card.classList = 'd-flex card';
+  card.classList = 'd-flex card card-' + project.cardId;
   workSection.appendChild(card);
   // Title
   const cardTitle = document.createElement('h3');
+  cardTitle.innerHTML = project.name;
   cardTitle.classList = 'card-title';
   card.appendChild(cardTitle);
   // Text
   const cardText = document.createElement('p');
+  cardText.innerHTML = project.description;
   cardText.classList = 'card-text';
   card.appendChild(cardText);
   // List
   const cardList = document.createElement('ul');
   cardList.classList = 'tag-container';
-  const listItem = document.createElement('li');    // Iterate over technologies
-  listItem.classList = 'card-tag';                  // to create many tags
-  cardList.appendChild(listItem);
+  // List elements
+  project.technologies.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = item;                      // Iterate over technologies
+    listItem.classList = 'card-tag';                // to create many tags
+    cardList.appendChild(listItem);
+  });
   card.appendChild(cardList);
   // Button
   const cardButton = document.createElement('button');
+  cardButton.innerHTML = "See Project";
   cardButton.classList = 'card-button';
   cardButton.setAttribute('type', 'button');
   card.appendChild(cardButton);
+
+  return(card)
 }
